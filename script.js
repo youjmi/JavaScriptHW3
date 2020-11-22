f81e43a4d6cf7616d1bb87917231b85125404814
Young Ji Kim <youjmik@gmail.com>
2020-11-21 22:55:15 -0500

    minor adjustment. shifted prompts to appear AFTER btn clicked

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = lowerCase.toUpperCase();
 var number = "123456789";
 //Gabe mentioned this at the last segment of class to find a way to combo all//
-var createPasswordComboArray =""
+var createPasswordComboArray = ""
 //Choosing character preferences (lower case..upper case...)//
 var chosenChars = ""
 //Range of Password Length//
-var passwordlength =  {from:8, to:128}
-
+var passwordlength = { from: 8, to: 128 }
 //Var Definitions//
 var chooseSymbols
 var chooselowerCase
@@ -23,38 +22,34 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Beging Loops until the correct amount (a number between 8-128) is added. Additional prompts to confirm Character Preference//
-passwordlength= (prompt("How many characters would you like your Password to be? Must be betweem 8 to 128 characters"));{
-  while (isNaN(passwordlength) || (passwordlength < 8 || passwordlength >128 )){
-    passwordlength = parseInt(prompt("Please make sure you put a NUMBER that is between 8 and 128"))
-  }
+function generatePassword() {
+  //Beging Loops until the correct amount (a number between 8-128) is added. Additional prompts to confirm Character Preference//
+  passwordlength = (prompt("How many characters would you like your Password to be? Must be betweem 8 to 128 characters")); {
+    while (isNaN(passwordlength) || (passwordlength < 8 || passwordlength > 128)) {
+      passwordlength = parseInt(prompt("Please make sure you put a NUMBER that is between 8 and 128"))
+    }
     console.log(passwordlength)
 
     chooseSymbols = confirm("Do you want Symbols?")
     console.log(chooseSymbols)
-  
-    chooselowerCase = confirm ("Do you want Lower Case letters?")
-    console.log(chooselowerCase)
-  
-    chooseupperCase = confirm ("Do you want Upper Case letters?")
-    console.log(chooseupperCase)
-  
-    chooseNumber = confirm("Do you want numbers?")
-    console.log(chooseNumber)}
 
-  //begin combo choices//
-function generatePassword()
-{
-  createPasswordComboArray=""
+    chooselowerCase = confirm("Do you want Lower Case letters?")  
+    console.log(chooselowerCase)
+
+    chooseupperCase = confirm("Do you want Upper Case letters?")
+    console.log(chooseupperCase)
+
+    chooseNumber = confirm("Do you want numbers?")
+    console.log(chooseNumber)
+  }
+//begin combo choices.. referenced this sequence from YouTube Video //
+
+  createPasswordComboArray = ""
   if (chooseSymbols === true) {
     chosenChars += specialSymbols;
   }
@@ -67,8 +62,8 @@ function generatePassword()
   if (chooseNumber === true) {
     chosenChars += number
   }
-    for (var i = 0; i < passwordlength; i++) {
-      createPasswordComboArray += chosenChars.charAt(Math.random() * chosenChars.length);
-    }
-    return createPasswordComboArray
+  for (var i = 0; i < passwordlength; i++) {
+    createPasswordComboArray += chosenChars.charAt(Math.random() * chosenChars.length);
   }
+  return createPasswordComboArray
+}
