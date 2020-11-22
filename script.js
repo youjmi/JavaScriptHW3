eb11c5f4efc73f5f96cacfdefe5081edfca33f6a
Young Ji Kim <youjmik@gmail.com>
2020-11-22 00:08:21 -0500

    updating comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//Testing Active Status//
 console.log("Generator is Active")
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Var Characters//
@@ -9,11 +9,11 @@ var upperCase = lowerCase.toUpperCase();
 var number = "123456789";
 //Gabe mentioned this at the last segment of class to find a way to combo all//
 var finalcomboPassword = ""
-//Choosing character preferences (lower case..upper case...)//
+//Choosing character preferences after popup confirm (lower case..upper case...)//
 var chosenChars = ""
 //Range of Password Length//
 var passwordlength = { from: 8, to: 128 }
-//Var Definitions//
+//Var Definitions to minimize usage of 'var'//
 var chooseSymbols
 var chooselowerCase
 var chooseupperCase
@@ -28,49 +28,49 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
+generateBtn.addEventListener("click", writePassword); {
 
-function generatePassword() {
-  //Beging Loops until the correct amount (a number between 8-128) is added. Additional prompts to confirm Character Preference//
-  passwordlength = (prompt("How many characters would you like your Password to be? Must be betweem 8 to 128 characters")); {
-    //Happened to find out the usage of 'while' by chance. I was stuck on this part for awhile because I wanted my prompts to continue even if a value wasn't correctly added. Instructor was reviewing a JS CheatSheet and as he scrolled through loops, I found 'while.'
-    while (isNaN(passwordlength) || (passwordlength < 8 || passwordlength > 128)) {
-      passwordlength = parseInt(prompt("Please make sure you put a NUMBER that is between 8 and 128"))
-    } console.log(passwordlength)
+  function generatePassword() {
+    //Beging Loops until the correct amount (a number between 8-128) is added. Additional prompts to confirm Character Preference//
+    passwordlength = (prompt("How many characters would you like your Password to be? Must be betweem 8 to 128 characters")); {
+      //Happened to find out the usage of 'while' by chance. I was stuck on this part for awhile because I wanted my prompts to continue even if a value wasn't correctly added. Instructor was reviewing a JS CheatSheet and as he scrolled through loops, I found 'while.'
+      while (isNaN(passwordlength) || (passwordlength < 8 || passwordlength > 128)) {
+        passwordlength = parseInt(prompt("Please make sure you put a NUMBER that is between 8 and 128"))
+      } console.log(passwordlength)
 
-    //begin combo choices.. referenced this sequence from YouTube Video //
-    finalcomboPassword = ""
+      //begin combo choices.. Gathered the IF Sequences from YT and combined it with my confirm pop-up //
+      finalcomboPassword = ""
 
-    chooseSymbols = confirm("Do you want Symbols?")
-    console.log(chooseSymbols)
-    if (chooseSymbols === true) {
-      chosenChars += specialSymbols;
+      chooseSymbols = confirm("Do you want Symbols?")
+      console.log(chooseSymbols)
+      if (chooseSymbols === true) {
+        chosenChars += specialSymbols;
+      }
+
+      chooselowerCase = confirm("Do you want Lower Case letters?")
+      console.log(chooselowerCase)
+      if (chooselowerCase === true) {
+        chosenChars += lowerCase;
+      }
+
+      chooseupperCase = confirm("Do you want Upper Case letters?")
+      console.log(chooseupperCase)
+      if (chooseupperCase === true) {
+        chosenChars += upperCase
+      }
+
+      chooseNumber = confirm("Do you want numbers?")
+      console.log(chooseNumber)
+      if (chooseNumber === true) {
+        chosenChars += number
+      }
     }
-
-    chooselowerCase = confirm("Do you want Lower Case letters?")  
-    console.log(chooselowerCase)
-    if (chooselowerCase === true) {
-      chosenChars += lowerCase;
+    //Random Math statement combining all to generate final product//
+    for (var i = 0; i < passwordlength; i++) {
+      finalcomboPassword += chosenChars.charAt(Math.random() * chosenChars.length);
+      //tracking method of showing random generator in progress//
+      console.log(finalcomboPassword)
     }
-
-    chooseupperCase = confirm("Do you want Upper Case letters?")
-    console.log(chooseupperCase)
-    if (chooseupperCase === true) {
-      chosenChars += upperCase
-    }
-
-    chooseNumber = confirm("Do you want numbers?")
-    console.log(chooseNumber)
-    if (chooseNumber === true) {
-      chosenChars += number
+    return finalcomboPassword
   }
-    }
-
-  for (var i = 0; i < passwordlength; i++) {
-    finalcomboPassword += chosenChars.charAt(Math.random() * chosenChars.length);
-    //tracking method of showing random generator in progress//
-    console.log(finalcomboPassword)
-  }
-  return finalcomboPassword
-}
 }
