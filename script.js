60ed2a539c6571d2006f545cd2f1dc563bf4f7b2
Young Ji Kim <youjmik@gmail.com>
2020-11-23 19:50:51 -0500

    finalized

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ console.log("Generator is Active")
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Var Characters//
-var specialSymbols = "~!@#$%^&*()_+";
+var specialSymbols = "~!@#$%^&*()_+".split()
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = lowerCase.toUpperCase();
-var number = "123456789";
-//Gabe mentioned this at the last segment of class to find a way to combo all//
-var finalcomboPassword = ""
-//Choosing character preferences after popup confirm (lower case..upper case...)//
-var chosenChars = ""
+var upperCase = lowerCase.toUpperCase()
+lowerCase.split()
+upperCase.split()
+var number = "123456789".split();
+//Gabe mentioned this like 3 classes ago... To be able to find a way to combine//
+var chosenChars = []
 //Range of Password Length//
 var passwordlength = { from: 8, to: 128 }
 //Var Definitions to minimize usage of 'var'//
@@ -19,7 +19,7 @@ var chooselowerCase
 var chooseupperCase
 var chooseNumber
 
-var emptyArr = []
+
 
 // Write password to the #password input//
 //DO NOT TOUCH. Separate from rest//
@@ -38,11 +38,11 @@ function writePassword() {
       } console.log(passwordlength)
 
       //begin combo choices.. Gathered the IF Sequences from YT and combined it with my confirm pop-up //
-      finalcomboPassword = ""
+      passwordText = ""
 
       chooseSymbols = confirm("Do you want Symbols?")
       console.log(chooseSymbols)
-      if (chooseSymbols === true) {
+      if (chooseSymbols=== true) {
         chosenChars += specialSymbols;
       } else chooseSymbols = false
 
@@ -63,15 +63,20 @@ function writePassword() {
       if (chooseNumber === true) {
         chosenChars += number
       } else chooseNumber = false
+
+     if (chosenChars.length ===0 ) {
+      chosenChars = alert ("No options selected. Please select ONE")
+     } else{ for (var i = 0; i < passwordlength; i++) {
+      passwordText += chosenChars.charAt(Math.floor(Math.random() * chosenChars.length));
+      //tracking method of showing random generator in progress//
+      console.log(passwordText)}
     }
     //Random Math statement combining all to generate final product//
-    for (var i = 0; i < passwordlength; i++) {
-      finalcomboPassword += chosenChars.charAt(Math.floor(Math.random() * chosenChars.length));
-      //tracking method of showing random generator in progress//
-      console.log(finalcomboPassword)
+    return passwordText
     }
-    return finalcomboPassword
   }
+  chosenChars =[]
+  console.log (chosenChars)
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
