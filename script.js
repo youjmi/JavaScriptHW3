69440cd75b5fe1df6cb268852adc27bd992b519d
Young Ji Kim <youjmik@gmail.com>
2020-11-24 01:20:37 -0500

    guarantee options

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ console.log("Generator is Active")
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Var Characters//
-var specialSymbols = "~!@#$%^&*()_+".split()
+var specialSymbols = "~!@#$%^&*()_+".split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = lowerCase.toUpperCase()
-lowerCase.split()
-upperCase.split()
-var number = "123456789".split();
+var upperCase = lowerCase.toUpperCase("")
+lowerCase.split("")
+upperCase.split("")
+var number = "123456789".split("");
 //Gabe mentioned this like 3 classes ago... To be able to find a way to combine//
-var chosenChars = []
+var chosenChars = ""
 //Range of Password Length//
 var passwordlength = { from: 8, to: 128 }
 //Var Definitions to minimize usage of 'var'//
@@ -19,6 +19,7 @@ var chooselowerCase
 var chooseupperCase
 var chooseNumber
 
+var finalOpts = ""
 
 
 // Write password to the #password input//
@@ -42,44 +43,60 @@ function writePassword() {
 
       chooseSymbols = confirm("Do you want Symbols?")
       console.log(chooseSymbols)
-      if (chooseSymbols=== true) {
+      if (chooseSymbols === true) {
         chosenChars += specialSymbols;
-      } else chooseSymbols = false
+        // var random= Math.floor(Math.random() * specialSymbols.length)
+        finalOpts += specialSymbols[1]
+        // console.log(random)
+        //console.log(specialSymbols)
+      }
+
+///******* math.floor & Remove , */
+
 
       chooselowerCase = confirm("Do you want Lower Case letters?")
       console.log(chooselowerCase)
       if (chooselowerCase === true) {
         chosenChars += lowerCase;
-      } else chooselowerCase = false
+        finalOpts += lowerCase[1]
+      }
 
       chooseupperCase = confirm("Do you want Upper Case letters?")
       console.log(chooseupperCase)
       if (chooseupperCase === true) {
         chosenChars += upperCase
-      } else chooseupperCase = false
+        finalOpts += upperCase[1]
+      }
 
       chooseNumber = confirm("Do you want numbers?")
       console.log(chooseNumber)
       if (chooseNumber === true) {
         chosenChars += number
-      } else chooseNumber = false
+        console.log(number)
+        finalOpts += number[1]
+      }
 
-     if (chosenChars.length ===0 ) {
-      chosenChars = alert ("No options selected. Please select ONE")
-     } else{ for (var i = 0; i < passwordlength; i++) {
-      passwordText += chosenChars.charAt(Math.floor(Math.random() * chosenChars.length));
-      //tracking method of showing random generator in progress//
-      console.log(passwordText)}
-    }
-    //Random Math statement combining all to generate final product//
-    return passwordText
+      console.log(finalOpts)
+
+      if (chosenChars.length === 0) {
+        chosenChars = alert("No options selected. Please select ONE")
+      } else {
+        for (var i = 0; i < passwordlength; i++) {
+          passwordText += chosenChars.charAt(Math.floor(Math.random() * chosenChars.length));
+          //tracking method of showing random generator in progress//
+          console.log(passwordText)
+        }
+      }
+      passwordText = finalOpts + passwordText
+      passwordText = passwordText.slice (0,passwordlength) //<-- look this up//
+      //Random Math statement combining all to generate final product//
+      return passwordText
     }
   }
-  chosenChars =[]
-  console.log (chosenChars)
+  finalOpts = []
+  chosenChars = []
+  console.log(chosenChars)
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-
 }
-
